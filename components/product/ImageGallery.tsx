0724f2de1ce5c46ca0ded2c157da2bfddad02f0c
MgHtetMyatAung <htetmyat@innovix-solutions.com>
2025-01-03 15:15:46 +0630

    add query in url

--- components/product/ImageGallery.tsx
+++ components/product/ImageGallery.tsx
@@ -1,11 +1,18 @@
 "use client";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 export default function ImageGallery({ images }: any) {
-  const [selectedImage, setSelectedImage] = React.useState(images[0]);
-  const changeImage = (image: string) => {
+  const searchParams = useSearchParams();
+
+  const imageId = searchParams.get("image");
+  const [selectedImage, setSelectedImage] = React.useState(
+    images[Number(imageId)] || images[0]
+  );
+  const changeImage = (image: any, index: any) => {
     setSelectedImage(image);
+    window.history.pushState({}, "", `?image=${index}`);
   };
   return (
     <div>
@@ -13,14 +20,14 @@ export default function ImageGallery({ images }: any) {
         <Image src={selectedImage} alt={"img"} width={500} height={500} />
       </div>
       <div className=" flex gap-3 mt-2">
-        {images.map((image: string) => (
+        {images.map((image: string, index: number) => (
           <Image
             key={image}
             src={image}
             alt={"img"}
             width={100}
             height={100}
-            onClick={() => changeImage(image)}
+            onClick={() => changeImage(image, index)}
             className={`${
               selectedImage === image
                 ? "border-2 border-blue-500 "
